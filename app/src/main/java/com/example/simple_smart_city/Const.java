c3be0ab6f82d842159b58d1fe61660bd7119cc1b
dxxupup <dxxupup@qq.com>
2021-12-12 11:32:02 +0800

    ""

--- app/src/main/java/com/example/simple_smart_city/Const.java
+++ app/src/main/java/com/example/simple_smart_city/Const.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Const {
     private static String IP = "http://124.93.196.45:10091";
-    private String TOKEN = "  ";
+    private String TOKEN = "null";
 
     public static String getIP() {
         return IP;
