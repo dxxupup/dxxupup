8196da4d3dc056cbbb0969e9ea3107627d8303d2
dxxupup <dxxupup@qq.com>
2021-12-15 16:30:21 +0800

    "update"

--- app/src/main/java/com/example/simple_smart_city/API.java
+++ app/src/main/java/com/example/simple_smart_city/API.java
@@ -6,6 +6,7 @@ import com.example.simple_smart_city.bean_p.ServerListBean;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface API {
 
@@ -15,6 +16,9 @@ public interface API {
     @GET("/prod-api/api/service/list?sRecommend=Y")
     Call<ServerListBean> serverList();
 
-    @GET("prod-api/press/press/list")
+    @GET("/prod-api/press/press/list")
     Call<ProjectBean> project();
+
+    @GET("/prod-api/press/press/list")
+    Call<ProjectBean> getNews(@Query("type") String id);
 }
--- app/src/main/java/com/example/simple_smart_city/view/HomeFragment.java
+++ app/src/main/java/com/example/simple_smart_city/view/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.simple_smart_city.view;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,6 +38,8 @@ public class HomeFragment extends Fragment {
     private FragHomeBinding binding;
     private MainActivity mainActivity;
     private final API req = Const.getReq();
+    private RecyclerView.Adapter newsListAdap;
+    private List<ProjectBean.RowsDTO> newsList;
 
     @Nullable
     @Override
@@ -51,6 +54,84 @@ public class HomeFragment extends Fragment {
         initBanner();
         initServerList();
         initProject();
+        initNewsList();
+        binding.z1.setOnClickListener(v -> {
+            updateNewsList("9");
+            changeColor((TextView) v);
+        });
+        binding.z2.setOnClickListener(v -> {
+            updateNewsList("17");
+            changeColor((TextView) v);
+        });
+        binding.z3.setOnClickListener(v -> {
+            updateNewsList("19");
+            changeColor((TextView) v);
+        });
+        binding.z4.setOnClickListener(v -> {
+            updateNewsList("20");
+            changeColor((TextView) v);
+        });
+        binding.z5.setOnClickListener(v -> {
+            updateNewsList("21");
+            changeColor((TextView) v);
+        });
+        binding.z6.setOnClickListener(v -> {
+            updateNewsList("22");
+            changeColor((TextView) v);
+        });
+    }
+
+    private void changeColor(TextView view){
+        binding.z1.setTextColor(Color.BLACK);
+        binding.z2.setTextColor(Color.BLACK);
+        binding.z3.setTextColor(Color.BLACK);
+        binding.z4.setTextColor(Color.BLACK);
+        binding.z5.setTextColor(Color.BLACK);
+        binding.z6.setTextColor(Color.BLACK);
+        view.setTextColor(Color.RED);
+    }
+    private void updateNewsList(String id) {
+        new Thread(() -> {
+            try {
+                newsList = req.getNews(id).execute().body().getRows();
+                mainActivity.runOnUiThread(() -> newsListAdap.notifyDataSetChanged());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    private void initNewsList() {
+        binding.newsList.setLayoutManager(new LinearLayoutManager(mainActivity));
+
+        new Thread(() -> {
+            try {
+                newsList = req.getNews("9").execute().body().getRows();
+                newsListAdap = new RecyclerView.Adapter<NewsListHolder>() {
+                    @NonNull
+                    @Override
+                    public NewsListHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        return new NewsListHolder(LayoutInflater.from(mainActivity).inflate(R.layout.item_news, parent, false));
+                    }
+
+                    @Override
+                    public void onBindViewHolder(@NonNull NewsListHolder holder, int position) {
+                        holder.context.setText(newsList.get(position).getContent());
+                        holder.title.setText(newsList.get(position).getTitle());
+                        Glide.with(mainActivity).load(Const.getIP() + newsList.get(position).getCover()).apply(RequestOptions.bitmapTransform(new RoundedCorners(20))).into(holder.img);
+                    }
+
+                    @Override
+                    public int getItemCount() {
+                        return newsList.size();
+                    }
+                };
+                mainActivity.runOnUiThread(() -> binding.newsList.setAdapter(newsListAdap));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
     }
 
     private void initProject() {
@@ -166,6 +247,18 @@ public class HomeFragment extends Fragment {
             super(itemView);
             text = itemView.findViewById(R.id.text);
             img = itemView.findViewById(R.id.img);
+        }
+    }
+
+    private static class NewsListHolder extends RecyclerView.ViewHolder {
+        ImageView img;
+        TextView title, context;
+
+        public NewsListHolder(@NonNull View itemView) {
+            super(itemView);
+            img = itemView.findViewById(R.id.img);
+            title = itemView.findViewById(R.id.title);
+            context = itemView.findViewById(R.id.context);
         }
     }
 }
